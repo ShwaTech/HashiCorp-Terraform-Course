20eff1d4b66179919db2a51010214aa9ac597e1c
ShwaTech <shwakesh13579@gmail.com>
2025-09-25 15:02:11 +0300

    Terraform - deponds_on meta-argument

--- 02 Output Block/Output-Block-Methods.tf
+++ 02 Output Block/Output-Block-Methods.tf
@@ -53,3 +53,64 @@ output "SHWA_STRING" {
   # sensitive = true
 }
 
+
+# ========================
+# deponds_on meta-argument
+# ========================
+
+resource "aws_vpc" "shwa_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "shwa_subnet" {
+  vpc_id     = aws_vpc.shwa_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "SHWA_Subnet"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.shwa_vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.shwa_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_instance" "shwa_ec2" {
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.shwa_subnet.id
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+
+  tags = {
+    Name = "SHWA_Instance"
+  }
+}
+
+output "ec2_public" {
+  value = aws_instance.shwa_ec2.public_ip
+  depends_on = [ aws_vpc_security_group_ingress_rule.allow_tls ]
+}
+
+
